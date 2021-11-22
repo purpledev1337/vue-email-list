8337d90eecd45da0a0713b1a8d73848f5d62dfbf
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-11-22 14:27:29 +0100

    for loop inside mounted function + v-for in html

---
+++ main.js
@@ -0,0 +1,32 @@
+// Attraverso l’apposita API di Boolean
+//  https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email
+// e stamparli in pagina all’interno di una lista. (sempre untilizzando Vue)
+// Bonus:
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+var app = new Vue ({
+    el: "#mail_list",
+    data: {
+        // changeble number to set the list length
+        listLength: 10,
+        // empty objects list
+        mailList: [
+
+        ]
+    },
+    mounted: function() {
+        const self = this;
+        // in every iteration will get api data and push into self.mailList
+        for (let i = 0; i < self.listLength; i++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(function (generateMail) {
+                    newMail = generateMail.data.response;
+                    self.mailList.push(newMail);
+                })
+        }
+    }
+})
+
+// while (self.mailList.length < 10){}
